31245ca903c831f81754fdb71154d6b8a488f9a9
王帅 <hisoftws@hotmail.com>
2021-02-23 17:02:39 +0800

    增加CRC系列验证4-64

---
+++ General/Validate/CRC/CRC4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace General.Validate.CRC
+{
+    public static class CRC4
+    {
+        public static byte ToCRC4(this byte[] data, int length)
+        {
+            byte i;
+            byte crc = 0;
+            int index = 0;
+            while (length-- > 0)
+            {
+                crc ^= data[index];
+                index++;
+                for (i = 0; i < 8; ++i)
+                {
+                    if ((crc & 1) == 1)
+                        crc = (byte)((crc >> 1) ^ 0x0C);
+                    else
+                        crc = (byte)(crc >> 1);
+                }
+            }
+            return crc;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using General.Validate.CRC;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Hexresult = new byte[] { 0xff, 0xa0 }.ToModbusCRC16(true);//General.Validate.CRC.CRC16.ToModbusCRC16(, true);
+            var result2 = new byte[] { 0xff, 0xa0 }.ToCRC16(true);//General.Validate.CRC.CRC16.crc16(,2);
+            var rec = new byte[] { 0xff, 0xa0 }.crc16();
+            var rec1 = Convert.ToInt16(Hexresult,16);
+
+            while (true){
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
